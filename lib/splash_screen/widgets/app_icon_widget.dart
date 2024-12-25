c029cf1f2c2e4481504164af23901b91d3a4b9ce
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-12-25 07:36:40 +0000

    Code update: Minor changes

--- lib/splash_screen/widgets/app_icon_widget.dart
+++ lib/splash_screen/widgets/app_icon_widget.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/cupertino.dart';
 
 class AppIconWidget extends StatelessWidget {
-  const AppIconWidget({super.key});
+  final double containerWidth;
+  const AppIconWidget({super.key, required this.containerWidth});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width * 0.3,
+      width: MediaQuery.of(context).size.width * containerWidth,
       padding: const EdgeInsets.all(8.0),
       child: const Image(image: AssetImage("assets/logo/54vibes-logo.png")),
     );
