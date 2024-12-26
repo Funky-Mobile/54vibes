227600f9982a92fbfe273da5854ddbc2efdd0778
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-12-26 08:22:49 +0000

    Code update: Minor changes

--- lib/screens/auth_screen/authentication_screen.dart
+++ lib/screens/auth_screen/authentication_screen.dart
@@ -1,4 +1,7 @@
+import 'package:fifty_four_vibes/screens/auth_screen/orientations/authentication_screen_landscape.dart';
 import 'package:flutter/cupertino.dart';
+
+import 'orientations/authentication_screen_portrait.dart';
 
 class AuthenticationScreen extends StatelessWidget {
   const AuthenticationScreen({super.key});
@@ -6,15 +9,19 @@ class AuthenticationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return OrientationBuilder(builder: (context, orientation) {
-      if (orientation == Orientation.portrait) {
 
+      //display the portrait view if the device screen if below 800
+      if(MediaQuery.of(context).size.width < 800) {
+        return const AuthenticationScreenPortrait();
+      }
+
+      if (orientation == Orientation.portrait) {
         // return the portrait view
-        return Text("PORTRAIT");
+        return const AuthenticationScreenPortrait();
 
       } else {
-
         // return the landscape view
-        return Text("LANDSCAPE");
+        return const AuthenticationScreenLandscape();
 
       }
     });
