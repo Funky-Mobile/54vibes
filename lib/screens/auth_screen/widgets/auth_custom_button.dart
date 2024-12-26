0501f09e8951bf095fa4af929329bc975d6897ac
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-12-26 08:22:59 +0000

    Code update: Minor changes

---
+++ lib/screens/auth_screen/widgets/auth_custom_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../../configures/app_buttons.dart';
+
+class AuthIconButton extends StatelessWidget {
+
+  final String buttonText;
+  final String icon;
+  final double iconWidth;
+
+  const AuthIconButton({super.key, required this.buttonText, required this.icon, required this.iconWidth});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: AppIconButton(buttonText: buttonText, icon: icon, iconWidth: iconWidth),
+        ),
+      ],
+    );
+  }
+}
+
+class AuthOutlinedButton extends StatelessWidget {
+
+  final String buttonText;
+  final Color buttonBackgroundColor;
+
+  const AuthOutlinedButton({super.key, required this.buttonText, required this.buttonBackgroundColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(child: OutlinedAppButton(buttonText: buttonText, buttonBackgroundColor: buttonBackgroundColor, isLandscape: false)),
+      ],
+    );
+  }
+}
