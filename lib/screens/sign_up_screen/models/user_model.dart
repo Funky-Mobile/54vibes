124250808ccbc1e245cea3765a74850222460d20
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2025-01-02 17:40:27 +0000

    Code update: Minor changes

---
+++ lib/screens/sign_up_screen/models/user_model.dart
@@ -0,0 +1,9 @@
+class UserModel {
+  String? email;
+  String? password;
+  String? dob;
+  String? gender;
+  String? fullName;
+
+  UserModel({this.email, this.password, this.dob, this.gender, this.fullName});
+}
